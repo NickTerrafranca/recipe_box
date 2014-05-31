d13605552d3558e7f009474b02591563ece4ec50
Nicholas Terrafranca <nterrafranca@gmail.com>
2014-05-31 16:28:59 -0400

    Working route to /recipe/:id. Core requirements complete

--- server.rb
+++ server.rb
@@ -11,37 +11,25 @@ def db_connection
 end
 
 get '/recipes' do
-  query = "SELECT recipes.id, recipes.name AS recipe_name, recipes.instructions, recipes.description, ingredients.name
+  query = "SELECT recipes.id, recipes.name AS recipe_name
   FROM recipes
-  JOIN ingredients
-  ON ingredients.recipe_id = recipes.id
-  ORDER BY recipes.name
-  LIMIT 10;"
+  WHERE recipes.instructions IS NOT NULL
+  ORDER BY recipes.name;"
   @recipes = db_connection do |conn|
     conn.exec_params(query)
   end
   erb :'index'
 end
 
-
-
-
-
-
-
-
-# get '/actors/:id' do
-#   id = params[:id]
-#   query = "SELECT movies.title, movies.id AS movie_id, cast_members.character, actors.name, actors.id
-#     FROM movies
-#     JOIN cast_members
-#     ON cast_members.movie_id = movies.id
-#     JOIN actors
-#     ON cast_members.actor_id = actors.id
-#     WHERE actors.id = $1
-#     ORDER BY movies.title;"
-#   @actor_movies = db_connection do |conn|
-#     conn.exec_params(query, [id])
-#   end
-#   erb :'actors/show'
-# end
+get '/recipes/:id' do
+  recipe_id = params[:id]
+  query = "SELECT recipes.id, recipes.name AS recipe_name, recipes.instructions, recipes.description, ingredients.name
+  FROM recipes
+  JOIN ingredients
+  ON ingredients.recipe_id = recipes.id
+  WHERE recipe_id = $1;"
+  @recipe_details = db_connection do |conn|
+    conn.exec_params(query, [recipe_id])
+  end
+  erb :'/show'
+end
